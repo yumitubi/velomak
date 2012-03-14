7b9e2358f913e4ad3fc42ee5ad2305b6b99e2969
le087 <mak.tomilov@gmail.com>
2012-03-14 22:09:03 +0600

    добавление подсветки кода

---
+++ velomak/blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from velomak.blog.models import Posts, Category, Comments
+
+admin.site.register(Posts)
+admin.site.register(Category)
+admin.site.register(Comments)
