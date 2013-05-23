cd958f56f5270a0dff7a51cb293cde03c392a9f8
Mak Tomilov <mak.tomilov@gmail.com>
2013-05-23 12:25:45 +0600

    fix style guide

--- velomak/blog/views.py
+++ velomak/blog/views.py
@@ -3,7 +3,11 @@ from django.template import RequestContext
 from django.http import Http404
 from django.shortcuts import render_to_response
 from forms import CommentForm
-from velomak.blog.utils import get_posts, get_tags, get_categs, get_posts_categ, get_posts_tag, get_tag_to_post, get_post, get_posts_section, get_sections, get_categs_section, save_comment, get_comments, add_capcha_code, search_in_db
+from velomak.blog.utils import get_posts, get_tags, get_categs, get_posts_categ
+from velomak.blog.utils import get_posts_tag, get_tag_to_post, get_post
+from velomak.blog.utils import get_posts_section, get_sections
+from velomak.blog.utils import save_comment, get_comments, add_capcha_code
+from velomak.blog.utils import search_in_db, get_categs_section
 import capcha
 
 
@@ -19,19 +23,20 @@ def blog(request):
     cloud_tags = get_tags()
     section_posts = get_sections()
     return render_to_response('titul.html', {
-        'current_page':current_page,
-        'header_list':header_list,
-        'tags_obr':tags_obr,
-        'categ_obr':categ_obr,
-        'cloud_tags':cloud_tags,
-        'meta':meta,
-        'section_posts':section_posts,
-        }, context_instance = RequestContext(request))
+                              'current_page': current_page,
+                              'header_list': header_list,
+                              'tags_obr': tags_obr,
+                              'categ_obr': categ_obr,
+                              'cloud_tags': cloud_tags,
+                              'meta': meta,
+                              'section_posts': section_posts,
+                              }, context_instance=RequestContext(request))
+
 
 def cur_post(request, offset):
     """Отображает один пост по URL
     Arguments:
-    - `request`: 
+    - `request`:
     - `offset`: урл из адресной строки
     """
     try:
@@ -41,12 +46,12 @@ def cur_post(request, offset):
     valid_add_comment = False
     meta = tags_obr = get_tag_to_post(offset)
     header_post = get_post(current_page)
-    if header_post == False:
+    if header_post is False:
         current_page = 'Страница не найдена'
         return render_to_response('404.html', {
-                'current_page':current_page,
-                'meta':meta,
-                }, context_instance = RequestContext(request))
+                                  'current_page': current_page,
+                                  'meta': meta,
+                                  }, context_instance=RequestContext(request))
     categ_obr = get_categs()
     cloud_tags = get_tags()
     section_posts = get_sections()
@@ -57,35 +62,37 @@ def cur_post(request, offset):
     if request.method == 'POST':
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
-            valid_add_comment = save_comment({'author':comment_form.cleaned_data['author'],
-                                              'email':comment_form.cleaned_data['email'],
-                                              'message':comment_form.cleaned_data['message'],
-                                              'post':current_page,
-                                              'delete':False,
-                                              'capcha_code':comment_form.cleaned_data['valid'],
-                                              })
-            if valid_add_comment == False:
+            valid_add_comment = save_comment(
+                {'author': comment_form.cleaned_data['author'],
+                 'email': comment_form.cleaned_data['email'],
+                 'message': comment_form.cleaned_data['message'],
+                 'post': current_page,
+                 'delete': False,
+                 'capcha_code': comment_form.cleaned_data['valid'],
+                 })
+            if valid_add_comment is False:
                 from django.core.mail import send_mail
-                send_mail(comment_form.cleaned_data['author'], 
-                          u'Новый комментарий на сайте: \n\n' + comment_form.cleaned_data['message'], 
-                          'mak.tomilov@yandex.ru', ['mak.tomilov@gmail.com'], 
+                send_mail(comment_form.cleaned_data['author'],
+                          u'Новый комментарий на сайте: \n\n' + comment_form.cleaned_data['message'],
+                          'mak.tomilov@yandex.ru', ['mak.tomilov@gmail.com'],
                           fail_silently=False)
                 comment_form = CommentForm()
     else:
         comment_form = CommentForm()
     return render_to_response('post.html', {
-        'current_page':current_page,
-        'header_post':header_post,
-        'tags_obr':tags_obr,
-        'categ_obr':categ_obr,
-        'cloud_tags':cloud_tags,
-        'meta':meta,
-        'section_posts':section_posts,
-        'comment_form':comment_form,
-        'comments':comments,
-        'name_capcha':name_capcha,
-        'valid_add_comment':valid_add_comment
-        }, context_instance = RequestContext(request))
+                              'current_page': current_page,
+                              'header_post': header_post,
+                              'tags_obr': tags_obr,
+                              'categ_obr': categ_obr,
+                              'cloud_tags': cloud_tags,
+                              'meta': meta,
+                              'section_posts': section_posts,
+                              'comment_form': comment_form,
+                              'comments': comments,
+                              'name_capcha': name_capcha,
+                              'valid_add_comment': valid_add_comment
+                              }, context_instance=RequestContext(request))
+
 
 def cur_tag(request, offset):
     """Отображает список материалов по выбранному тегу
@@ -102,14 +109,15 @@ def cur_tag(request, offset):
     cloud_tags = get_tags()
     section_posts = get_sections()
     return render_to_response('titul.html', {
-            'current_page':current_page,
-            'header_list':header_list,
-            'tags_obr':tags_obr,
-            'categ_obr':categ_obr,
-            'cloud_tags':cloud_tags,
-            'meta':meta,
-            'section_posts':section_posts 
-            }, context_instance = RequestContext(request))
+                              'current_page': current_page,
+                              'header_list': header_list,
+                              'tags_obr': tags_obr,
+                              'categ_obr': categ_obr,
+                              'cloud_tags': cloud_tags,
+                              'meta': meta,
+                              'section_posts': section_posts
+                              }, context_instance=RequestContext(request))
+
 
 def cur_categ(request, offset):
     """Отображает материалы по текущей категории
@@ -123,22 +131,23 @@ def cur_categ(request, offset):
     header_list = get_posts_categ(offset_without_earth)
     # categ_obr - получаем категории, которые принадлежат
     # только к определенной категории
-    if header_list and header_list[0].section: 
-        categ_obr = get_categs_section(header_list[0].section) 
+    if header_list and header_list[0].section:
+        categ_obr = get_categs_section(header_list[0].section)
     else:
         categ_obr = get_categs()
     tags_obr = get_tags()
     cloud_tags = get_tags()
     section_posts = get_sections()
     return render_to_response('titul.html', {
-        'current_page':current_page,
-        'header_list':header_list,
-        'categ_obr':categ_obr,
-        'tags_obr':tags_obr,
-        'cloud_tags':cloud_tags,
-        'meta':meta,
-        'section_posts':section_posts 
-        }, context_instance = RequestContext(request))
+                              'current_page': current_page,
+                              'header_list': header_list,
+                              'categ_obr': categ_obr,
+                              'tags_obr': tags_obr,
+                              'cloud_tags': cloud_tags,
+                              'meta': meta,
+                              'section_posts': section_posts
+                              }, context_instance=RequestContext(request))
+
 
 def cur_section(request, offset):
     """ Обображает материалы для конкретной секции
@@ -152,17 +161,18 @@ def cur_section(request, offset):
     cloud_tags = get_tags()
     section_posts = get_sections()
     return render_to_response('titul.html', {
-        'current_page':current_page,
-        'header_list':header_list,
-        'categ_obr':categ_obr,
-        'tags_obr':tags_obr,
-        'cloud_tags':cloud_tags,
-        'meta':meta,
-        'section_posts':section_posts
-        }, context_instance = RequestContext(request))
-    
+                              'current_page': current_page,
+                              'header_list': header_list,
+                              'categ_obr': categ_obr,
+                              'tags_obr': tags_obr,
+                              'cloud_tags': cloud_tags,
+                              'meta': meta,
+                              'section_posts': section_posts
+                              }, context_instance=RequestContext(request))
+
+
 def search(request):
-    """return results for search 
+    """return results for search
     Arguments:
     - `request`:
     """
@@ -176,11 +186,10 @@ def search(request):
     else:
         header_list = []
     return render_to_response('titul.html', {
-        'current_page':current_page,
-        'meta':meta,
-        'section_posts':section_posts,
-        'categ_obr':categ_obr,
-        'cloud_tags':cloud_tags,
-        'header_list':header_list
-        }, context_instance = RequestContext(request))
- 
+                              'current_page': current_page,
+                              'meta': meta,
+                              'section_posts': section_posts,
+                              'categ_obr': categ_obr,
+                              'cloud_tags': cloud_tags,
+                              'header_list': header_list
+                              }, context_instance=RequestContext(request))
