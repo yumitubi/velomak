09c46449aec7d8666393bd3326008ba2f6a19aa9
Mak Tomilov <mak.tomilov@gmail.com>
2013-06-07 23:29:31 +0600

    добавил возможность писать в макрдаун комменты

--- velomak/blog/forms.py
+++ velomak/blog/forms.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django import forms
-from tinymce.widgets import TinyMCE 
+from markitup.widgets import MarkItUpWidget
 
 class CommentForm(forms.Form):
     author = forms.CharField(label='Ваше имя:', max_length=100)
     email = forms.EmailField(label='E-mail:', max_length=100)
     #message = forms.CharField(label='Комменатрий:', widget=forms.Textarea)
-    message = forms.CharField(label='Комменатрий:', widget=TinyMCE(attrs={'cols':70, 'rows': 10}))
+    message = forms.CharField(label='Комменатрий:', widget=MarkItUpWidget())
     valid = forms.CharField(label='Вы робот?', max_length=30)
 
